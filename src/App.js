14a8d2f3946cdf81fd7e1890735f636d050dedc3
Alper Tugriçeri <atugriceri@globalit.com.tr>
2024-10-19 09:41:29 +0300

    route setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import PageRoutes from "./routes";
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+import PageRoutes from './routes';
+import Logo from './assets/images/logo.png';
+import Navbar from './components/navbar';
+import PageWrapper from './components/page-wrapper';
+import ErrorBoundary from './components/error-boundary';
 
 function App() {
   return (
-    <PageRoutes />
+    <Provider store={store}>
+      <Navbar>
+        <a href="/products" tabIndex="0">
+          <img src={Logo} alt="Company Logo" style={{ height: '24px', aspectRatio: 1 }} />
+        </a>
+      </Navbar>
+
+      <ErrorBoundary>
+        <PageWrapper>
+          <PageRoutes />
+        </PageWrapper>
+      </ErrorBoundary>
+    </Provider>
   );
 }
 
